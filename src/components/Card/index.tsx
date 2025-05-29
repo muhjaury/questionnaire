289c8a635f1447d12963c0225dc24c0ba26181e1
Muhammad Jaury <muhjaury@gmail.com>
2025-05-30 00:49:56 +0700

    Install and setup AOS

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,6 +1,9 @@
 function Card({ ...props }: any) {
   return (
-    <div className="bg-gray-300 rounded-3xl py-8 px-4 md:p-8 w-full max-w-300 mb-4">
+    <div
+      className="bg-gray-300 rounded-3xl py-8 px-4 md:p-8 w-full max-w-300 mb-4"
+      data-aos="fade-up"
+    >
       {props.children}
     </div>
   );
--- src/layout/CoreLayout.tsx
+++ src/layout/CoreLayout.tsx
@@ -1,4 +1,15 @@
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+
 function CoreLayout({ ...props }: any) {
+  useEffect(() => {
+    AOS.init({
+      duration: 500,
+      once: true,
+    });
+  }, []);
+
   return (
     <div className="py-8 px-4 md:py-8 md:px-32 flex flex-col justify-center min-h-screen">
       {props.children}
